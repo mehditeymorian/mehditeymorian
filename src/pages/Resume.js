bcbdb67290580b40bf66105d6e7410ec48d42559
Mehdi <mehditeymorian322@gmail.com>
2022-08-12 22:10:51 +0430

    feat: remove education from work experience

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,16 +3,11 @@ import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
-import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
 import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
 
-import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
-import ta from '../data/resume/ta';
 import { skills, categories } from '../data/resume/skills';
 
 const sections = [
@@ -41,11 +36,8 @@ const Resume = () => (
 
         </div>
       </header>
-      <Education data={degrees} />
       <Experience data={positions} title="Experience" />
-      <Experience data={ta} title="Teaching Assistant" />
       <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
       <References />
 
     </article>
