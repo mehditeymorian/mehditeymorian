e17db4305b435f7be54b38013d089a4e83bc3747
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-08-26 15:05:12 +0430

    feat: add image gallery component

---
+++ src/components/general/ImageGallery.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Image = ({ data }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const split = data.image.split('/');
+  const alt = split[split.length - 1];
+
+  const onImageClick = () => {
+    setClicked((prevState) => !prevState);
+  };
+
+  return (
+    <div>
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+      <img
+        onClick={onImageClick}
+        className={clicked ? 'gallery-img-clicked' : 'gallery-img'}
+        src={`${process.env.PUBLIC_URL}${data.image}`}
+        alt={alt}
+      />
+    </div>
+  );
+};
+
+Image.propTypes = {
+  data: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const ImageGallery = ({ data }) => (
+  <div className="gallery">
+    {data.map((each, index) => <Image data={each} key={index} />)}
+  </div>
+);
+
+ImageGallery.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.array.isRequired,
+};
+
+export default ImageGallery;
