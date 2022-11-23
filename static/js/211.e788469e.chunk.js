e969ca652c868e8036e55b65ac4e7646214a4951
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-11-23 20:25:53 +0330

    Updates

--- static/js/211.e788469e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[211],{211:function(e,c,n){n.r(c);n(2791);var a=n(4806),i=n(184);c.default=function(){return(0,i.jsx)(a.Z,{title:"Academic Background",description:"Learn about Mehdi Teymorian's Academic Background.",children:(0,i.jsx)("a",{children:"Academic Background"})})}}}]);
-//# sourceMappingURL=211.e788469e.chunk.js.map
